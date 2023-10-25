d1431c372821cab25f7a0664348acf4912674c31
Matías Paz <neoxap@gmail.com>
2023-10-25 20:49:10 -0300

    Primer escritura

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+var valorEnDolar = 155.5;
+
+var cotizacionEnPesos = 16.893;
+
+var valorEnPesos = valorEnDolar * cotizacionEnPesos;
+
+valorEnPesos = valorEnPesos.toFixed(2);
+
+alert ("$"+valorEnPesos);
