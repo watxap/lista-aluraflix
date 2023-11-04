a16f09f396deac9e8459bc51fecbb57e99f398a2
Matías Paz <neoxap@gmail.com>
2023-11-04 02:39:59 -0300

    Correcciones varias

--- js/javascript.js
+++ js/javascript.js
@@ -3,10 +3,9 @@ var listaPeliculas = [
     "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
     "https://lumiere-a.akamaihd.net/v1/images/esh25_584x800_022aaeef.jpeg",
     "https://lumiere-a.akamaihd.net/v1/images/p_findingnemo_19752_05271d3f.jpeg",
-    "https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG"
-  ];
-  
+    "https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG",
+  ]; 
+
   for (var i = 0; i < listaPeliculas.length; i++) {
     document.write("<img src=" + listaPeliculas[i] + ">");
   }
-  
