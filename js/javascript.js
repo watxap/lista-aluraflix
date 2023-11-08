93a219ae3dc66995f880a889a12cd94bf46a912f
Matías Paz <neoxap@gmail.com>
2023-11-08 17:51:31 -0300

    Desafíos completados

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,47 @@
-var imagenPeliculas = [
-    "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg",
-    "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
-    "https://lumiere-a.akamaihd.net/v1/images/esh25_584x800_022aaeef.jpeg",
-    "https://lumiere-a.akamaihd.net/v1/images/p_findingnemo_19752_05271d3f.jpeg",
-    "https://www.universalpictures.com.ar/tl_files/content/movies/fastx/poster/01.jpg",
-  ]; 
+var posterPeliculas = [];
 
-var tituloPeliculas = [
-  "Escuela de Rock","Arrival","El Jóven Manos de Tijera","Buscando a Nemo","Rápidos y Furiosos X"
-]
+var tituloPeliculas = [];
 
-//aprovechar funcionalidad y agregar variable tituloPeliculas
-imagenPeliculas.push('https://lumiere-a.akamaihd.net/v1/images/p_insideout_19751_af12286c.jpeg', 'https://cdn.shopify.com/s/files/1/1057/4964/products/harry-potter-and-the-chamber-of-secrets-vintage-movie-poster-original-1-sheet-27x41.jpg');
+var trailerPeliculas = [];
 
 var i = 0;
 
-while (i < imagenPeliculas.length){
-  if ((imagenPeliculas[i].endsWith('jpg')) || (imagenPeliculas[i].endsWith('jpeg'))){
- 
- document.write(
-      "<div style='display: inline-block; margin:4%; color:white'>" +
-        "<img src= " + imagenPeliculas[i] + ">" + 
-        "<h5>" + tituloPeliculas[i] +"</h5>" +
-        "</div>"
-    );
-    i++;
- }else{alert('Error - Formatos de imagen admitidas: jpg/jpeg'); break; }}
+function agregarPeliculas (tituloPelicula,posterPelicula,trailerPelicula) {
+  if (
+    !tituloPeliculas.includes(tituloPelicula) &&
+    (posterPelicula.endsWith("jpg") || posterPelicula.endsWith("jpeg")) &&
+    (trailerPelicula.startsWith("https://") || trailerPelicula.startsWith("http://"))
+  ) {
+    tituloPeliculas.push(tituloPelicula);
+    posterPeliculas.push(posterPelicula);
+    trailerPeliculas.push(trailerPelicula);
+  } else if (tituloPeliculas.includes(tituloPelicula)) {
+    alert(`La película ${tituloPelicula} ya existe en la lista.`);
+  } else {
+    alert(`La URL de las imagenes deben finalizar en .jpg o .jpeg`);
+  }
+}
+
+agregarPeliculas (
+  "Escuela de Rock",
+  "https://es.web.img2.acsta.net/pictures/15/10/23/15/24/061018.jpg",
+  "https://www.youtube.com/watch?v=alXk2rMCrNc");
+
+agregarPeliculas (
+  "Arrival",
+  "https://m.media-amazon.com/images/I/91UusfCtQaL._AC_UF1000,1000_QL80_.jpg",
+  "https://www.youtube.com/watch?v=tFMo3UJ4B4g",
+);
+agregarPeliculas (
+  "El Jóven Manos de Tijera",
+  "https://images.squarespace-cdn.com/content/v1/5f1811b47a7edc2eea4892b4/1628529323411-DNUOGJ9XBBKEVYVAIUM0/EDWARD+SCISSORHANDS+POSTER.jpg",
+  "https://www.youtube.com/watch?v=TBHIO60whNw"
+);
+
+while (i < posterPeliculas.length){
+   
+  document.write(
+      `<div style='display: inline-block; margin:2%; color:white'><a href=${trailerPeliculas[i]} target="_blank"><img src="${posterPeliculas[i]}"></a><p>${tituloPeliculas[i]}</p></div>`
+      );
+      i++;
+   }
